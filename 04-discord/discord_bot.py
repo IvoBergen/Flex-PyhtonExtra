eb33bd672f9bcb6401fa991a123372d78f389bf0
Ivo <34885@ma-web.nl>
2022-10-27 10:08:42 +0200

    Ik changed the code so the bot can respond!

--- 04-discord/discord_bot.py
+++ 04-discord/discord_bot.py
@@ -2,12 +2,25 @@ import discord
 intents = discord.Intents.default()
 intents.messages= True 
 client = discord.Client(intents=intents)
-bottoken=open("C:/Users/ivobe/Desktop/Ma/Bewijzenmap/periode1.1/flex/extraphyton/04-discord/Geheim/Discordbot.txt", "r").readline()
+bottoken=open("C:/Users/ivobe/Desktop/Geheim/Discordbot.txt", "r").readline()
 
 @client.event
 async def on_ready():
     guild = client.guilds[0]
     print(guild.name, "is the name of the server")
     print(client.user, "has connected to the client")
+    channel = guild.text_channels[0]
+    print(channel.name, "Is the name of the channel!")
+    await channel.send("I'm online!")
+
+@client.event
+async def on_message(message):
+    print(message.channel.name, "the message was posted from this channel")
+    print(message.content)
+    print(message.author,"is the user who wrote the message")
+    print(message.created_at,"is when the message was posted")
+    print(message.channel,"is the channel this message was posted in")
+    if message.author.bot == False:
+        await message.channel.send("Hello "+ str(message.author))
 
 client.run(bottoken)
